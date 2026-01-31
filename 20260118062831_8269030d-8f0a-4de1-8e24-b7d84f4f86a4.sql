e26acca13149c4cd0f2b53c4f7812e6be2244296
gaurav-11-cyber <rajgr6299530@gmail.com>
2026-01-31 07:45:11 +0530

    Delete 20260118062831_8269030d-8f0a-4de1-8e24-b7d84f4f86a4.sql

--- 20260118062831_8269030d-8f0a-4de1-8e24-b7d84f4f86a4.sql
+++
@@ -1,3 +0,0 @@
--- Fix function search path for security
-ALTER FUNCTION public.update_updated_at_column() SET search_path = public;
-ALTER FUNCTION public.handle_new_user() SET search_path = public;
